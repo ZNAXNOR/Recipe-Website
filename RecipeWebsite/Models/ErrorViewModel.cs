35477d02c37394a70b367a6f0f39fdbb6368e516
surendra dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-11-29 14:27:22 +0530

    Add project files.

---
+++ RecipeWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RecipeWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
