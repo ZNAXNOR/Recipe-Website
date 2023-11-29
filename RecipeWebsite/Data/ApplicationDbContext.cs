ce935663fe49fdf5a237c8ab71ff1012ee0227bb
ZNAXNOR <121810601+ZNAXNOR@users.noreply.github.com>
2023-11-29 15:07:51 +0530

    Created Database context and installed NuGet packages.
    Created: ApplicationDbContext
    
    Installed: bootstrap, Microsoft.AspNetCore.Identity.EntityFrameworkCore

---
+++ RecipeWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RecipeWebsite.Models;
+
+namespace RecipeWebsite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CollectionModel> Collections { get; set; }
+        public DbSet<PostModel> Posts { get; set; }
+    }
+}
