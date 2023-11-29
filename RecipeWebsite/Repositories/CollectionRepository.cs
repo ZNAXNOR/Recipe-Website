16609afbbfb5a750e7eccce98058d1948c198e3e
ZNAXNOR <omkarsdalvi1@gmail.com>
2023-11-29 21:50:03 +0530

    Created Repositories
    Created: CollectionRepository, PostRepository

---
+++ RecipeWebsite/Repositories/CollectionRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeWebsite.Data;
+using RecipeWebsite.Interfaces;
+using RecipeWebsite.Models;
+
+namespace RecipeWebsite.Repositories
+{
+    public class CollectionRepository : ICollectionInterface
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CollectionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(CollectionModel collection)
+        {
+            _context.Add(collection);
+            return Save();
+        }
+
+        public bool Delete(CollectionModel collection)
+        {
+            _context.Remove(collection);
+            return Save();
+        }
+
+        public async Task<IEnumerable<CollectionModel>> GetAll()
+        {
+            return await _context.Collections.ToListAsync();
+        }
+
+        public async Task<CollectionModel> GetByIdAsync(int id)
+        {
+            return await _context.Collections.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<CollectionModel> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Collections.AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(CollectionModel collection)
+        {
+            _context.Update(collection);
+            return Save();
+        }
+    }
+}
---
+++ RecipeWebsite/Repositories/PostRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeWebsite.Data;
+using RecipeWebsite.Interfaces;
+using RecipeWebsite.Models;
+
+namespace RecipeWebsite.Repositories
+{
+    public class PostRepository : IPostInterface
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(PostModel post)
+        {
+            _context.Add(post);
+            return Save();
+        }
+
+        public bool Delete(PostModel post)
+        {
+            _context.Remove(post);
+            return Save();
+        }
+
+        public async Task<IEnumerable<PostModel>> GetAll()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        public async Task<PostModel> GetByIdAsync(int id)
+        {
+            return await _context.Posts.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<PostModel> GetByIdAsyncNoTracking(int id)
+        {
+            return await _context.Posts.AsNoTracking().FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(PostModel post)
+        {
+            _context.Update(post);
+            return Save();
+        }
+    }
+}
