3d9db2e23996c4dba5793998fb62d49f6dadb078
ZNAXNOR <omkarsdalvi1@gmail.com>
2023-11-29 22:43:16 +0530

    Created Services
    Created: EmailService, PhotoService

---
+++ RecipeWebsite/Services/EmailSenderService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using RecipeWebsite.Helpers;
+using System.Net.Mail;
+using System.Net;
+using RecipeWebsite.Interfaces;
+
+namespace RecipeWebsite.Services
+{
+    public class EmailSenderService : IEmailSenderInterface
+    {
+        private readonly ILogger<EmailSenderService> _logger;
+
+        public EmailHelper Options { get; set; }
+        public EmailSenderService(IOptions<EmailHelper> options, ILogger<EmailSenderService> logger)
+        {
+            Options = options.Value;
+            _logger = logger;
+        }
+
+        public async Task SendEmailAsync(string Email, string Subject, string Message)
+        {
+            if (string.IsNullOrEmpty(Options.Host) || string.IsNullOrEmpty(Options.Username) || string.IsNullOrEmpty(Options.Password))
+            {
+                throw new Exception("Null SMTP_Credientials");
+            }
+            await Execute(Options.Host, Options.Username, Options.Password, Email, Subject, Message);
+        }
+
+        private Task Execute(string host, string username, string password, string toEmail, string subject, string message)
+        {
+            var client = new SmtpClient(host, 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(username, password)
+            };
+            return client.SendMailAsync(
+                   new MailMessage(from: "DoNotReply < " + username + " > ",
+                                   to: toEmail,
+                                   subject,
+                                   message
+                                   ));
+        }
+    }
+}
---
+++ RecipeWebsite/Services/PhotoService.cs
@@ -0,0 +1,46 @@
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Microsoft.Extensions.Options;
+using RecipeWebsite.Helpers;
+using RecipeWebsite.Interfaces;
+
+namespace RecipeWebsite.Services
+{
+    public class PhotoService : IPhotoInterface
+    {
+        private readonly Cloudinary _cloundinary;
+
+        public PhotoService(IOptions<CloudinarySettingsHelper> config)
+        {
+            var acc = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+                );
+            _cloundinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Crop("fill").Gravity("face")
+                };
+                uploadResult = await _cloundinary.UploadAsync(uploadParams);
+            }
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicUrl)
+        {
+            var publicId = publicUrl.Split('/').Last().Split('.')[0];
+            var deleteParams = new DeletionParams(publicId);
+            return await _cloundinary.DestroyAsync(deleteParams);
+        }
+    }
+}
