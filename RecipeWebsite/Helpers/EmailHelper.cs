cf03b4fa972fcbc4d7501fcfa1bc6fcd8bae01bb
ZNAXNOR <omkarsdalvi1@gmail.com>
2023-11-29 21:43:43 +0530

    Created Helpers
    Created: CloudinaryHelper, EmailHelper

---
+++ RecipeWebsite/Helpers/EmailHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeWebsite.Helpers
+{
+    public class EmailHelper
+    {
+        public string? Host { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
