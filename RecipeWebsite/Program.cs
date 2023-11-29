e9f16a99b48bb9c572421fc6fd79de6c3469eff9
ZNAXNOR <omkarsdalvi1@gmail.com>
2023-11-29 17:39:04 +0530

    Added Secrets, Updated Program.cs and installed NuGet packages.
    Added: secrets.json
    
    Updated: Program.cs for database and Identity
    
    Installed: Microsoft.EntityFrameworkCore, Microsoft.EntityFrameworkCore.SqlServer, Microsoft.EntityFrameworkCore.Tools

--- RecipeWebsite/Program.cs
+++ RecipeWebsite/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RecipeWebsite.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Database
+var MSSQLdatabase = builder.Configuration.GetConnectionString("Database_Connection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(MSSQLdatabase));
+
+// Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
